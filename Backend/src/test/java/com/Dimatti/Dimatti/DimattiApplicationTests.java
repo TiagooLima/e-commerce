e7f4e56225dbd790280e46d696cbaca45bd600bb
Diego Claudino <claudinodiego98@gmail.com>
2026-03-20 10:59:16 -0300

    Add files via upload

---
+++ Backend/src/test/java/com/Dimatti/Dimatti/DimattiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Dimatti.Dimatti;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DimattiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
