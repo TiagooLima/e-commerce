522249159b0ba47e17433ee2fd714a51c8ab994e
Matheus Santos <etelvino070@gmail.com>
2026-03-24 09:32:27 -0300

    Add files via upload

--- back-end_python/login_cadastro/conexao.py
+++ back-end_python/login_cadastro/conexao.py
@@ -9,4 +9,19 @@ def get_conexao():
         database="dimatti"
     )
 
-def cadastrar():
+def cadastrar(nome, email, telefone):
+    con = get_conexao
+    cursor = con.cursor()
+
+    sql = "INSERTO INTO usuarios(nome, email, telefone) VALUES (%s, %s, %s)"
+    dados = ("Joao", "hfgshfs@gmail.com", "12445679986")
+    cursor.execute(sql,dados (nome,email,telefone))
+    con.commit()
+    con.close()
+    print("Usuario cadastrado com sucesso!")
+
+def login():
+    con = get_conexao
+    curs
+
+    
