5283c42308946928c814944d8df290e5b7f38d38
Matheus Santos <etelvino070@gmail.com>
2026-03-25 11:01:14 -0300

    Add files via upload

--- back-end_python/login_cadastro/conexao.py
+++ back-end_python/login_cadastro/conexao.py
@@ -6,22 +6,68 @@ def get_conexao():
         host="localhost",
         user="root",
         password="univille",
-        database="dimatti"
+        database="dimatti",
+        
     )
 
-def cadastrar(nome, email, telefone):
-    con = get_conexao
+def cadastrar(nome, email, telefone, senha):
+    con = get_conexao()
     cursor = con.cursor()
 
-    sql = "INSERTO INTO usuarios(nome, email, telefone) VALUES (%s, %s, %s)"
-    dados = ("Joao", "hfgshfs@gmail.com", "12445679986")
-    cursor.execute(sql,dados (nome,email,telefone))
-    con.commit()
-    con.close()
-    print("Usuario cadastrado com sucesso!")
-
+    sql = "INSERT INTO usuarios (nome, email, telefone, senha) VALUES (%s, %s, %s, %s)"
+    valores = (nome, email, telefone, senha)
+    
+    try:
+        cursor.execute(sql, valores)
+        con.commit()
+        con.close()
+        print(f"Usuario {nome} cadastrado com sucesso!")
+        
+    except mysql.connector.Error as err:
+        print(f"Erro na: {err}")
+        
+    finally:
+        con.close()
+        cursor.close()   
+         
 def login():
-    con = get_conexao
-    curs
+    nome_digitado = input("Digite seu nome:")
+    senha_digitada = input("Digite sua senha:")
+    
+    con = get_conexao()
+    cursor = con.cursor()
+    
+    sql = "INSERT INTO usuarios WHERE nome = %s AND senha = %s"
+    dados = (nome_digitado, senha_digitada)
+    
+    cursor.execute(sql, dados)
+    
+    usuario = cursor.fetchone()
+    
+    if usuario:
+        print(f"Usuario logado com sucesso!, {usuario[0]}")
+        
+    else:
+        print("Morreu")
+        
+        con.close()
+        cursor.close()
+        
+    
+        
+        
+        
+    
+    
+        
+    
+        
+cadastrar("Tiago", "tiago@gmail.com", "12123456789", "123456789")
+cadastrar("Roanaldo", "ronaldo@gmail.com", "12123456789", "123456789")
+cadastrar("Diego", "diego@gmail.com", "12123456789", "123456789")
+
+
+
+
 
     
--- back-end_python/login_cadastro/dimatti_bd.sql
+++ back-end_python/login_cadastro/dimatti_bd.sql
@@ -6,12 +6,15 @@ CREATE TABLE usuarios(
 id INT AUTO_INCREMENT PRIMARY KEY,
 nome VARCHAR(100) NOT NULL,
 email VARCHAR(100) NOT NULL,
-telefone VARCHAR(11)
+telefone VARCHAR(11),
+senha VARCHAR(32) NOT NULL
 );
 
-INSERT INTO usuarios(nome, email, telefone) VALUES
-('Matheus', 'matheus@gmail.com', '47997188861'),
-('Ana', 'ana@gmail.com', '47123456789');
+INSERT INTO usuarios(nome, email, telefone, senha) VALUES
+('Matheus', 'matheus@gmail.com', '47997188861', '123456789'),
+('Ana', 'ana@gmail.com', '47123456789', '1234567789');
 
 
 SELECT * FROM usuarios;
+
+SELECT nome, senha FROM usuarios  WHERE nome = usuarios;
