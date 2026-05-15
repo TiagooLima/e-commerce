5a7457883c2cf29a4a8faaba524895b99f273686
Diego Claudino <claudinodiego98@gmail.com>
2026-05-15 13:16:40 -0300

    Delete produtosapi directory

--- produtosapi/src/main/java/produtos/produtosapi/enums/produtosEnums.java
+++
@@ -1,9 +0,0 @@
-package produtos.produtosapi.enums;
-
-public enum produtosEnums {
-    AGUARDANDO_CONFIRMACAO,
-    PEDIDO_CONFIRMADO,
-    PREPARANDO,
-    A_CAMINHO;
-
-}
--- produtosapi/src/main/java/produtos/produtosapi/repository/produtosRepository.java
+++
@@ -1,7 +0,0 @@
-package produtos.produtosapi.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import produtos.produtosapi.models.produtos;
-
-public interface produtosRepository extends JpaRepository<produtos, String> {
-}
